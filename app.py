2980b03269afc2c8eac468c9fe8a6ccf54d33c84
abuBakarBhatti <abubakar85040@gmail.com>
2023-10-15 16:47:00 +0500

    Flask app done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 
@@ -6,7 +6,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Python with Flask"
+    name = input("Enter your name: ")
+    data = {"name": name}
+    return render_template("home.html", data = {"name": name})
+
+@app.route("/about", methods=['GET', 'POST'])
+def about():
+
+    if request.method == 'POST':
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['numb2'])
+        result = num1 + num2
+        
+        return render_template("about.html", dicti={'result': result})
+    else:
+        return render_template("about.html", dicti={'result': 'result'})
 
 if __name__ == "__main__":
     app.run(debug=True)
